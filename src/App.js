57a79949d0ebf41b0a209511cc4be6ca8af64be0
Matthew Houston <matthouston027@gmail.com>
2023-08-17 14:18:21 -0500

    Add changeHandlers and manage state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { useState } from 'react';
+
 import Header from "./components/header/Header"
 import UserInput from "./components/UserInput/UserInput";
 import ResultsTable from "./components/ResultsTable/ResultsTable";
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
+
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    setUserInput(yearlyData);
+  };
 
-    const yearlyData = []; // per-year results
-
+  const yearlyData = []; // per-year results
+  if(userInput){
     let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput['expected-return'] / 100;
     const duration = +userInput['duration'];
-
+  
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
@@ -26,14 +30,12 @@ function App() {
         yearlyContribution: yearlyContribution,
       });
     }
-
-    // do something with yearlyData ...
-  };
+  }
 
   return (
     <div>
       <Header/>
-      <UserInput />
+      <UserInput onCalculate={calculateHandler}/>
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
